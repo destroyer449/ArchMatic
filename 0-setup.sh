89a3298c7b41858199501783d005f2e88de0997d
Dominic Adamson <adamson.dom@gmail.com>
2020-12-23 11:49:48 -0700

    removed extraneous commands

--- 0-setup.sh
+++ 0-setup.sh
@@ -14,13 +14,12 @@ if ! source install.conf; then
     printf "hostname="$hostname"\n" >> "install.conf"
     printf "username="$username"\n" >> "install.conf"
 
-    echo $hostname >> /etc/hostname
     echo "127.0.0.1     localhost\n" >> /etc/hosts
     echo "::1     localhost\n" >> /etc/hosts
     echo "127.0.0.1     "$hostname".localdomain     "$hostname"\n" >> /etc/hosts
 
-    useradd -mG wheel $username
-    passwd $username
+    useradd -mG wheel "$username"
+    passwd "$username"
 fi
 
 echo "-------------------------------------------------"
@@ -47,13 +46,13 @@ sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 timedatectl --no-ask-password set-timezone America/Denver
 timedatectl --no-ask-password set-ntp 1
-localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_COLLATE="" LC_TIME="en_US.UTF-8"
+localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
 
 # Set keymaps
 localectl --no-ask-password set-keymap us
 
 # Hostname
-hostnamectl --no-ask-password set-hostname $hostname
+hostnamectl --no-ask-password set-hostname "$hostname"
 
 # Add sudo no password rights
 sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
