f168c972807d7711f2fa6495a00caac4a44a780e
Dominic Adamson <adamson.dom@gmail.com>
2020-12-23 11:52:52 -0700

    changed formatting to be consistent

--- 1-base.sh
+++ 1-base.sh
@@ -12,58 +12,57 @@ echo -e "\nInstalling Base System\n"
 PKGS=(
 
     # --- XORG Display Rendering
-        'xorg'                  # Base Package
-        'xorg-drivers'          # Display Drivers
-        'xorg-server'           # XOrg Server
-        'xorg-apps'             # XOrg Apps Group
-        'xorg-xinput'           # Xorg Xinput
-        'mesa'                  # Open Source Version of OpenGL
-        'xdg-user-dis'          # Set Up XDG Directories
-        'intel-ucode'           # Intel Specific Stuff
+        'xorg'                    # Base Package
+        'xorg-drivers'            # Display Drivers
+        'xorg-server'             # XOrg Server
+        'xorg-apps'               # XOrg Apps Group
+        'xorg-xinput'             # Xorg Xinput
+        'mesa'                    # Open Source Version of OpenGL
+        'xdg-user-dis'            # Set Up XDG Directories
+        'intel-ucode'             # Intel Specific Stuff
 
     # --- Setup Desktop
-        'xmonad'                # Xmonad Window Manager
-        'trayer'                # Sys Tray
-        'xfce4-power-manager'   # Power Manager
-        'xclip'                 # System Clipboard
-        'lxsession'             # Elevate Applications
-        'lxappearance'          # Set System Themes
+        'trayer'                  # Sys Tray
+        'xfce4-power-manager'     # Power Manager
+        'xclip'                   # System Clipboard
+        'lxsession'               # Elevate Applications
+        'lxappearance'            # Set System Themes
 
     # --- Login Display Manager
-        'lightdm'                       # Base Login Manager
-        'light-locker'                  # LightDM screen locker
+        'xorg-xinit'              # Startx Command
+        'i3lock-fancy'            # Screen Locker
 
     # --- Networking Setup
-        'wpa_supplicant'            # Key negotiation for WPA Wireless Networks
-        'wireless_tools'            # Wireless tools
-        'dialog'                    # Enables Shell Scripts To Trigger Dialog Boxex
-        'network-manager-applet'    # System Tray Icon/Utility for Network Connectivity
-        'libsecret'                 # Library For Storing Passwords
+        'wpa_supplicant'          # Key negotiation for WPA Wireless Networks
+        'wireless_tools'          # Wireless tools
+        'dialog'                  # Enables Shell Scripts To Trigger Dialog Boxex
+        'network-manager-applet'  # System Tray Icon/Utility for Network Connectivity
+        'libsecret'               # Library For Storing Passwords
 
     # --- Audio
-        'alsa-utils'        # Advanced Linux Sound Architecture (ALSA) Components https://alsa.opensrc.org/
-        'alsa-plugins'      # ALSA Plugins
-        'pulseaudio'        # Pulse Audio Sound Components
-        'pulseaudio-alsa'   # ALSA Configuration for Pulse Audio
-        'pavucontrol'       # Pulse Audio Volume Control
-        'pnmixer'           # System Tray Volume Control
+        'alsa-utils'              # Advanced Linux Sound Architecture (ALSA) Components https://alsa.opensrc.org/
+        'alsa-plugins'            # ALSA Plugins
+        'pulseaudio'              # Pulse Audio Sound Components
+        'pulseaudio-alsa'         # ALSA Configuration for Pulse Audio
+        'pavucontrol'             # Pulse Audio Volume Control
+        'pnmixer'                 # System Tray Volume Control
 
     # --- Bluetooth
-        'bluez'                 # Daemons for the Bluetooth Protocol Stack
-        'bluez-utils'           # Bluetooth Development and Debugging Utilities
-        'bluez-firmware'        # Firmwares for Broadcom BCM203x and STLC2300 Bluetooth Chips
-        'blueberry'             # Bluetooth Configuration Tool
-        'pulseaudio-bluetooth'  # Bluetooth Support for PulseAudio
+        'bluez'                   # Daemons for the Bluetooth Protocol Stack
+        'bluez-utils'             # Bluetooth Development and Debugging Utilities
+        'bluez-firmware'          # Firmwares for Broadcom BCM203x and STLC2300 Bluetooth Chips
+        'blueberry'               # Bluetooth Configuration Tool
+        'pulseaudio-bluetooth'    # Bluetooth Support for PulseAudio
 
     # --- Printers
-        'cups'                  # Open Source Printer Drivers
-        'cups-pdf'              # PDF Support For Cups
-        'ghostscript'           # PostScript Interpreter
-        'gsfonts'               # Adobe Postscript Replacement Fonts
-        'hplip'                 # HP Drivers
-        'system-config-printer' # Printer Setup  Utility
-        'avahi'                 # Wireless Printer Resolution
-        'nss-mdns'              # Local Domain Resolution
+        'cups'                    # Open Source Printer Drivers
+        'cups-pdf'                # PDF Support For Cups
+        'ghostscript'             # PostScript Interpreter
+        'gsfonts'                 # Adobe Postscript Replacement Fonts
+        'hplip'                   # HP Drivers
+        'system-config-printer'   # Printer Setup  Utility
+        'avahi'                   # Wireless Printer Resolution
+        'nss-mdns'                # Local Domain Resolution
 )
 
 for PKG in "${PKGS[@]}"; do
